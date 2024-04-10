155b935122d5a993c7c957472b5b03e368dfb1d8
dangebalkrishna <166622658+dangebalkrishna@users.noreply.github.com>
2024-04-10 19:09:00 +0530

    Update i2c.h

--- i2c.h
+++ i2c.h
@@ -8,6 +8,14 @@ typedef enum {
     STATUS_ERROR
 } status_t;
 
+status_t i2c_init(void);
+status_t i2c_enable_interrupt_pin(void);
+status_t i2c_disable_interrupt_pin(void);
+status_t i2c_get_full_scale(uint8_t *full_scale);
+status_t i2c_get_output_data_rate(uint8_t *output_data_rate);
+status_t i2c_set_output_data_rate(uint8_t new_output_data_rate);
+status_t i2c_read_axis_data(uint8_t axis, int16_t *output_data);
+
 status_t i2c_read(
     uint8_t bus_address,
     uint8_t register_address,
